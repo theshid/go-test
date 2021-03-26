ae6443241ac70077b43b4f1b6b35536681070807
Shid Insa <theshid.insa@gmail.com>
2021-03-26 16:38:04 +0100

    commit

--- Iteration/repeat_test.go
+++ Iteration/repeat_test.go
@@ -7,12 +7,20 @@ import (
 
 func TestRepeat(t *testing.T){
 
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
 	t.Run("iteration 6 times", func(t *testing.T) {
 		repeated := Repeat("a",6)
 		expected := "aaaaaa"
-		if repeated != expected {
+		/*if repeated != expected {
 			t.Errorf("expected %q but got %q", expected, repeated)
-		}
+		}*/
+		assertCorrectMessage(t,repeated,expected)
 	})
 
 }
