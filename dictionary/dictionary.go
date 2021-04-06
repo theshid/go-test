96e0b06e346a66d87dfcdb3ecdd4360c7d372b68
Shid Insa <theshid.insa@gmail.com>
2021-04-06 02:31:20 +0100

    commit

---
+++ dictionary/dictionary.go
@@ -0,0 +1,11 @@
+package dictionary
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) string {
+	return d[word]
+}
+
+func Search(dictionary map[string]string, word string) string {
+	return dictionary[word]
+}
